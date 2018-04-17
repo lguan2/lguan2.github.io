bf26ef1d075d35bfce1036257dc0a8fb543d436b
lguan2 <guanlin152@hotmail.com>
2018-04-17 16:26:12 -0700

    update main.js

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 swal({
-    html: 'test',
-    allowOutsideClick: false,
-    allowEscapeKey: false,
-    confirmButtonText: "Cancel",
-  
-    onOpen: function () {
-      swal.showLoading();
-      swal.disableButtons();
-      setTimeout(swal.disableLoading, 'test'.length * 500)
-      setTimeout(swal.enableButtons, 'test'.length * 500) 
-    }
-  })
+  title: '<redirecting-text>' + 'Redirecting...' + '<redirecting-text>',
+  width: '262px',
+  height: '108px',
+  imageUrl: 'spinner.gif',
+  imageWidth: 48,
+  imageHeight: 48,
+  animation: true,
+  showConfirmButton: false,
+  onOpen: function () {
+    setTimeout(() => {
+      swal({
+          title: '<redirecting-title>' + 'REDIRECTION TIMED OUT' + '<redirecting-title>',
+          html: '<redirecting-text>' + 'Lorem ipsum dolor sir amet.' + '<redirecting-text>',
+          width: '262px',
+          height: '95px',
+          allowOutsideClick: false,
+          allowEscapeKey: false,
+          //confirmButtonClass: 'confirm-button',
+          confirmButtonText: '<button-text>' + 'OK' + '<button-text>',
+          buttonsStyling: false
+      });
+    }, 2000)
+  }
+})
